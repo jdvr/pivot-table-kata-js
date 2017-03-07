112b08ac288aaf6e53239a6d9d9c8a73b0160bb3
jdvega <jdvega@idealista.com>
2017-03-07 13:45:48 +0100

    green: The pivoted table should have the expected content

--- src/processing/pivot.js
+++ src/processing/pivot.js
@@ -7,18 +7,44 @@ const filter = require('lodash/fp').filter;
 const uniqWith = require('lodash/fp').uniqWith;
 const isEqual = require('lodash/fp').isEqual;
 const concat = require('lodash/fp').concat;
+const reduce = require('lodash/fp').reduce;
+const values = require('lodash/fp').values;
 
 const distinctValues = mapFunction => flow(
     map(mapFunction),
     uniqWith(isEqual)
 );
 
+function keyCalculatorWith(filterFunction) {
+    return flow(
+        filter(filterFunction),
+        reduce((key, cell) => key + cell.value, "")
+    );
+}
+
 function pivot(table, headerColumn, valueColumn) {
+    const filterCellFunction = cell => cell.name !== headerColumn && cell.name !== valueColumn;
+    const keyOf = keyCalculatorWith(filterCellFunction);
 
     const headers = concat(
         filter(header => header !== headerColumn && header !== valueColumn)(table.header),
         distinctValues(row => row.valueAtName(headerColumn))(table.rows)
     );
-    return new Table(headers, table.rows);
+
+    const rows = flow(
+        reduce((group, row) => {
+            const rowHash = keyOf(row.cells);
+            if (!group[rowHash]) {
+                group[rowHash] = filter(filterCellFunction)(row.cells);
+            }
+            group[rowHash].push(new Cell(row.valueAtName(headerColumn), row.valueAtName(valueColumn)));
+            return group;
+        }, {}),
+        map(values),
+        map(value => new Row(value))
+    )(table.rows);
+
+    return new Table(headers, rows);
 }
-module.exports = { pivot: pivot };
+
+module.exports = {pivot: pivot};
