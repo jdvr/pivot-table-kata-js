4a81b86ce53d793c8be98e91c9b7ae62fe54a2a7
jdvega <jdvega@idealista.com>
2017-03-07 10:08:37 +0100

    green: The pivoted table should have the expected number of columns - The pivoted table should have the expected header

--- src/processing/pivot.js
+++ src/processing/pivot.js
@@ -1,8 +1,23 @@
 var Table = require('../domain/table.js');
 var Row = require('../domain/row.js');
 var Cell = require('../domain/cell.js');
+var _ = require('lodash');
 
+function uniqValues(headerColumn, rows) {
+    var values = _.map(rows, function (row) {
+        return row.valueAtName(headerColumn)
+    });
+    return _.uniqWith(values, _.isEqual);
+}
+function removeElements(headers, headerColumn, valueColumn) {
+    return _.filter(headers, function (header) {
+        return header !== headerColumn && header !== valueColumn;
+    });
+}
 function pivot(table, headerColumn, valueColumn) {
-    return table;
+    var headers = _.concat(
+        removeElements(table.header, headerColumn, valueColumn),
+        uniqValues(headerColumn, table.rows));
+    return new Table(headers, table.rows);
 }
 module.exports = { pivot: pivot };
